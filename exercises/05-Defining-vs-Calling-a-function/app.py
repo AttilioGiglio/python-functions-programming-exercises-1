9d4c871f290363760111c6eb58c11e66336a9a67
Andres Sanchez <61630429+AttilioGiglio@users.noreply.github.com>
2020-09-15 17:22:19 -0300

    commit#5

--- exercises/05-Defining-vs-Calling-a-function/app.py
+++ exercises/05-Defining-vs-Calling-a-function/app.py
@@ -1,4 +1,7 @@
 # Define the function called "multi" that expects 2 parameters:
+def multi(numOne, numTwo):
+    result= numOne*numTwo
+    return result
 
 # don't edit anything below this line
 return_value = multi(7,53812212)
